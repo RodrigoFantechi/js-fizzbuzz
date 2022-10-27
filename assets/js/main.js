c3f03c3e15bbca62b7acb08384b88b43780337ca
RodrigoFantechi <rodrifante595@libero.it>
2022-10-27 16:06:18 +0200

    alternative method

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,19 @@ for (let i = 1; i <=100; i++) {
      } 
      console.log(varianteDAppoggio);
  }
+
+// oppure
+
+ for (let i = 1; i <=100; i++) {
+   
+ 
+    if ((i%3)===0 && (i%5)===0){
+        console.log('FizzBuzz');
+    } else if((i%3)===0){
+        console.log('Fizz');
+     } else if ((i%5)===0){
+        console.log('Buzz');
+     } else{
+     console.log(i);
+    }
+ }
